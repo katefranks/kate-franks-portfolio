52211fef6de7e6eeddebe760e5a673df4721aa0b
katefranks <kmfranks9@gmail.com>
2021-08-05 10:33:12 -0400

    implemented static image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -8,9 +10,15 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <h1>Kate Franks Portfolio</h1>
       <p>Software Developer</p>
+      <StaticImage
+        alt="Kate Franks"
+        src="../images/katefranks_about.JPG"
+      />
     </Layout>
   )
 }
 
 // Step 3: Export your component
 export default IndexPage
+
+// 8/4- trying to debug Gatsby CLI issue- on NMB docs page, trying to do #5.
