49597f198d384c3a31941b06f6118f745680a052
katefranks <kmfranks9@gmail.com>
2021-08-05 21:46:14 -0400

    implemented file system route api

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="Blog Posts">
+      <p>Blog post contents will go here (under construction).</p>
+    </Layout>
+  )
+}
+export default BlogPost
